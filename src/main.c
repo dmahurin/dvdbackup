fa995abb9ff2255962cfc2ff1bf34243ae27e111
drung <>
2008-03-07 16:02:47 +0000

    configure.ac: added bug report mailing list src/main.c: - extracted usage description to extra file Usage - added long options - added -V, --version option - made -i and -o options optional - changed default read error handling to skip multiple blocks

--- src/main.c
+++ src/main.c
@@ -18,176 +18,105 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "config.h"
 #include "dvdbackup.h"
 
 /* C standard libraries */
 #include <limits.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/* C POSIX library */
+/* C POSIX libraries */
 #include <sys/stat.h>
 #include <unistd.h>
 
 /* libdvdread */
 #include <dvdread/dvd_reader.h>
 
-
-/* Usage:
-	To gather info about the dvd:
-		dvdbackup -i /dev/dvd -I
-
-	Gerneral backup information:
-		If your backup directory is /my/dvd/backup/dir/
-		specified with the "-o" flag, then dvdbackup
-		will create a DVD-Video structure under
-		/my/dvd/backup/dir/TITLE_NAME/VIDEO_TS.
-
-		Since the title is "unique" you can use the same dir
-		for all your DVD backups. If it happens to have a
-		generic title dvdbackup will exit with a return value
-		of 2. And you will need to specify a title name with
-		the -n switch.
-
-		dvdbackup will always mimic the original DVD-Video
-		structure. Hence if you e.g. use the -M (mirror) you
-		will get an exact duplicate of the original. This
-		means that every file will be have the same size as
-		the original one. Like wise goes also for the -F
-		and the -T switch.
-
-		However the -t and (-t -s/-e) switch is a bit
-		different the titles sectors will be written to the
-		original file but not at the same offset as the
-		original one since they may be gaps in the cell
-		structure that we don't fill.
+/* other libraries */
+#include <getopt.h>
 
 
-	To backup the whole DVD
-
-		dvdbackup -M -i/dev/dvd -o/my/dvd/backup/dir/
-
-		This action creates a valid DVD-Video structure
-		that can be burned to a DVD-/+R(W) with help of
-		genisoimage
+/* String containing name the program is called with. */
+const char* program_name;
 
 
-	To backup the main feature of the DVD:
+static void print_version() {
+	printf("%s\n", PACKAGE_STRING);
 
-		dvdbackup -F -i/dev/dvd -o/my/dvd/backup/dir/
-
-		This action creates a valid DVD-Video structure
-		of the feature title set
-
-		dvdbackup defaults to get the 16:9 version of the
-		main feature if a 4:3 is also present on the DVD.
-		To get the 4:3 version use -a 0.
-
-		dvdbackup makes it best to make a inteligent guess
-		what is the main feature of the DVD - in case it fails
-		please send a bug report.
-
-
-	To backup a title set
-
-		dvdbackup -T 2 -i/dev/dvd -o/my/dvd/backup/dir/
-
-		where "-T 2" specifies that you want to backup
-		title set 2 i.e. all VTS_02_X.XXX files.
-
-		This action creates a valid DVD-Video structure
-		of the specified title set
-
-
-	To backup a title:
-
-		dvdbackup -t 1 -i/dev/dvd -o/my/dvd/backup/dir
-
-		This action backups all cells that forms the
-		specified title. Note that there can be sector
-		gaps in between one cell and an other. dvdbackup
-		will backup all sectors that belongs to the title
-		but will skip sectors that aren't a part of the title.
-
-
-	To backup a specific chapter or chapters from a title:
-
-		dvdbackup -t 1 -s 20 -e 25 -i/dev/dvd -o/my/dvd/backup/dir
-
-		This action will backup chapter 20 to 25 in title 1, as with
-		the backup of a title there can be sector gaps between one chapter
-		(cell) and on other.dvdbackup will backup all sectors that belongs
-		to the title 1 chapter 20 to 25 but will skip sectors that aren't
-		a part of the title 1 chapter 20 to 25.
-
-		To backup a single chapter e.g. chapter 20 do -s 20 -e 20
-		To backup from chapter 20 to the end chapter use only -s 20
-		To backup to chapter 20 from the first chapter use only -e 20
-
-		You can skip the -t switch and let the program guess the title although
-		it's not recommended.
-
-		If you specify a chapter that his higher than the last chapter of the title
-		dvdbackup will turncate to the highest chapter of the title.
-
-	Return values:
-		0 on success
-		1 on usage error
-		2 on title name error
-		-1 on failure
-
-
-	Todo - i.e. what's on the agenda.
-		Make the main feature guessing algoritm better. Not that it doesn't do
-		its job, but its implementation isn't that great. I would also like
-		to preserve more information about the main feature since that would
-		let me preform better implementations in other functions that depends
-		on the titles_info_t and title_set_info_t strcutures.
-
-		Make it possible to extract cells in a title not just chapters (very
-		easy so it will definitly be in the next version).
-
-		Make a split mirror (-S) option that divides a DVD-9 to two valid DVD-5
-		video structures. This is not a trivial hack and it's my main goal
-		the next month or so. It involves writing ifoedit and vobedit
-		libraries in order to be able to manipulate both the IFO structures
-		and the VOB files. Out of this will most probably also come tscreate
-		and vtscreate which will enable you to make a very simple DVD-Video
-		from MPEG-1/2 source.
-
-*/
-
-
-void usage(){
-	fprintf(stderr,"\nUsage: dvdbackup [options]\n");
-	fprintf(stderr,"\t-i device\twhere device is your dvd device\n");
-	fprintf(stderr,"\t-v X\t\twhere X is the amount of verbosity\n");
-	fprintf(stderr,"\t-I\t\tfor information about the DVD\n");
-	fprintf(stderr,"\t-o directory\twhere directory is your backup target\n");
-	fprintf(stderr,"\t-n name\t\t(optional) set the title - useful if autodetection fails\n");
-	fprintf(stderr,"\t-M\t\tbackup the whole DVD\n");
-	fprintf(stderr,"\t-F\t\tbackup the main feature of the DVD\n");
-	fprintf(stderr,"\t-T X\t\tbackup title set X\n");
-	fprintf(stderr,"\t-t X\t\tbackup title X\n");
-	fprintf(stderr,"\t-s X\t\tbackup from chapter X\n");
-	fprintf(stderr,"\t-e X\t\tbackup to chapter X\n");
-	fprintf(stderr,"\t-a 0\t\tto get aspect ratio 4:3 instead of 16:9 if both are present\n");
-	fprintf(stderr,"\t-r {a,b,m}\t\tselect read error handling: a=abort (default), b=skip block, m=skip multiple blocks\n");
-	fprintf(stderr,"\t-h\t\tprint a brief usage message\n");
-	fprintf(stderr,"\t-?\t\tprint a brief usage message\n\n");
-	fprintf(stderr,"\t-i is mandatory\n");
-	fprintf(stderr,"\t-o is mandatory except if you use -I\n");
-	fprintf(stderr,"\t-a is option to the -F switch and has no effect on other options\n");
-	fprintf(stderr,"\t-s and -e should preferably be used together with -t \n\n");
-	exit(1);
+	/* It is important to separate the year from the rest of the message,
+	 * as done here, to avoid having to retranslate the message when a new
+	 * year comes around.
+	 */
+	printf("Copyright (C) 2002 Olaf Beck <olaf_sc@yahoo.com>\n\
+Copyright (C) %s Benjamin Drung <benjamin.drung@gmail.com>\n\n\
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\
+This is free software: you are free to change and redistribute it.\n\
+There is NO WARRANTY, to the extent permitted by law.\n\
+Homepage: %s\n", "2008", "http://dvdbackup.sourceforge.net/");
 }
 
 
-int main(int argc, char *argv[]){
+static void print_help() {
+	/* TRANSLATORS: --help output 1 (synopsis) */
+	printf("Usage: %s [OPTION]...\n", program_name);
+
+	/* TRANSLATORS: --help output 2 (brief description)
+	   no-wrap */
+/*	fputs ("\
+Print a friendly, customizable greeting.\n", stdout); */
+	printf("\n");
+
+	/* TRANSLATORS: --help output 3: options 1/2
+	   no-wrap */
+	printf("\
+  -h, --help         display this help and exit\n\
+  -V, --version      display version information and exit\n\n");
+
+	/* TRANSLATORS: --help output 4: options 2/2
+	   no-wrap */
+	printf("\
+  -I, --info         prints information about the DVD\n\
+  -M, --mirror       backup the whole DVD\n\
+  -F, --feature      backup the main feature of the DVD\n\
+  -T, --titleset=X   backup title set X\n\
+  -t, --title=X      backup title X\n\
+  -s, --start=X      backup from chapter X\n\
+  -e, --end=X        backup to chapter X\n\n");
+
+	printf("\
+  -i, --input=DEVICE       where DEVICE is your dvd device\n\
+                           if not given /dev/dvd is used\n\
+  -o, --output=DIRECTORY   where directory is your backup target\n\
+                           if not given the current directory is used\n");
+	printf("\
+  -v, --verbose=X          where X is the amount of verbosity\n\
+  -n, --name=NAME          set the title (useful if autodetection fails)\n\
+  -a, --aspect=0           to get aspect ratio 4:3 instead of 16:9 if both are\n\
+                           present\n\
+  -r, --error={a,b,m}      select read error handling: a=abort, b=skip block,\n\
+                           m=skip multiple blocks (default)\n\n");
+	
+	printf("\
+  -a is option to the -F switch and has no effect on other options\n\
+  -s and -e should preferably be used together with -t\n");
+
+	printf("\n");
+	  /* TRANSLATORS: --help output 5 (end)
+	     TRANSLATORS: the placeholder indicates the bug-reporting address
+	     for this application.  Please add _another line_ with the
+	     address for translation bugs. */
+	printf ("Report bugs to <%s>.\n", PACKAGE_BUGREPORT);
+}
+
+
+int main(int argc, char* argv[]) {
 
 	/* Args */
 	int flags;
+	bool lose = false;
 
 	/* Switches */
 	int title_set = 0;
@@ -202,84 +131,80 @@ int main(int argc, char *argv[]){
 	int do_feature = 0;
 	int do_info = 0;
 
-	read_error_strategy_t errorstrat = STRATEGY_ABORT;
+	/* Because of copy protection you normally want to skip
+	 * the defect sectors. To speed things up we skip multiblocks.
+	 */
+	read_error_strategy_t errorstrat = STRATEGY_SKIP_MULTIBLOCK;
 
 	int return_code = 0;
 
 	/* DVD Video device */
-	char * dvd=NULL;
+	char* dvd = "/dev/dvd";
 
 	/* Temp switch helpers */
-	char * verbose_temp=NULL;
-	char * aspect_temp=NULL;
-	char * start_chapter_temp=NULL;
-	char * end_chapter_temp=NULL;
-	char * titles_temp=NULL;
-	char * title_set_temp=NULL;
-	char * errorstrat_temp=NULL;
+	char* verbose_temp = NULL;
+	char* aspect_temp = NULL;
+	char* start_chapter_temp = NULL;
+	char* end_chapter_temp = NULL;
+	char* titles_temp = NULL;
+	char* title_set_temp = NULL;
+	char* errorstrat_temp = NULL;
 
 
 	/* Title of the DVD */
-	char title_name[33]="";
-	char * provided_title_name=NULL;
+	char title_name[33] = "";
+	char* provided_title_name = NULL;
 
 	/* Targer dir */
-	char * targetdir=NULL;
+	char* targetdir = ".";
 
 	/* Temp filename,dirname */
 	char targetname[PATH_MAX];
 	struct stat fileinfo;
 
-
 	/* The DVD main structure */
-	dvd_reader_t *	_dvd=NULL;
+	dvd_reader_t* _dvd = NULL;
 
+	/* the long and the short options */
+	static const struct option longopts[] = {
+		{"help", no_argument, NULL, 'h'},
+		{"version", no_argument, NULL, 'V'},
+		
+		{"info", no_argument, NULL, 'I'},
+		{"mirror", no_argument, NULL, 'M'},
+		{"feature", no_argument, NULL, 'F'},
+		{"titleset", required_argument, NULL, 'T'},
+		{"title", required_argument, NULL, 't'},
+		{"start", required_argument, NULL, 's'},
+		{"end", required_argument, NULL, 'e'},
 
+		{"input", required_argument, NULL, 'i'},
+		{"output", required_argument, NULL, 'o'},
+		{"verbose", required_argument, NULL, 'v'},
+		{"name", required_argument, NULL, 'n'},
+		{"aspect", required_argument, NULL, 'a'},
+		{"error", required_argument, NULL, 'r'},
+		{NULL, 0, NULL, 0}
+	};
+	const char* shortopts = "hVIMFT:t:s:e:i:o:v:n:a:r:";
+	
+	program_name = argv[0];
 
 	/* TODO: do isdigit check */
-
-	while ((flags = getopt(argc, argv, "MFI?hi:v:a:o:n:s:e:t:T:r:")) != -1) {
-		switch (flags) {
-		case 'r':
-			if(optarg[0]=='-') usage();
-			errorstrat_temp=optarg;
+	
+	while((flags = getopt_long(argc, argv, shortopts, longopts, NULL)) != -1) {
+		switch(flags) {
+		case 'h':
+			print_help();
+			exit(EXIT_SUCCESS);
+			break;
+		case 'V':
+			print_version();
+			exit(EXIT_SUCCESS);
 			break;
 
-		case 'i':
-      			if(optarg[0]=='-') usage();
-      			dvd = optarg;
-      			break;
-		case 'v':
-			if(optarg[0]=='-') usage();
-			verbose_temp = optarg;
-			break;
-		case 'o':
-			if(optarg[0]=='-') usage();
-			targetdir = optarg;
-			break;
-		case 'n':
-      			if(optarg[0]=='-') usage();
-      			provided_title_name = optarg;
-      			break;
-		case 'a':
-			if(optarg[0]=='-') usage();
-			aspect_temp = optarg;
-			break;
-		case 's':
-			if(optarg[0]=='-') usage();
-			start_chapter_temp = optarg;
-			break;
-		case 'e':
-			if(optarg[0]=='-') usage();
-			end_chapter_temp = optarg;
-			break;
-		case 't':
-			if(optarg[0]=='-') usage();
-			titles_temp = optarg;
-			break;
-		case 'T':
-			if(optarg[0]=='-') usage();
-			title_set_temp = optarg;
+		case 'I':
+			do_info = 1;
 			break;
 		case 'M':
 			do_mirror = 1;
@@ -287,40 +212,64 @@ int main(int argc, char *argv[]){
 		case 'F':
 			do_feature = 1;
 			break;
-		case 'I':
-			do_info = 1;
+		case 'T':
+			title_set_temp = optarg;
+			break;
+		case 't':
+			titles_temp = optarg;
+			break;
+		case 's':
+			start_chapter_temp = optarg;
+			break;
+		case 'e':
+			end_chapter_temp = optarg;
+			break;
+			
+		case 'i':
+			dvd = optarg;
+      		break;
+		case 'o':
+			targetdir = optarg;
+			break;
+		case 'v':
+			verbose_temp = optarg;
+			break;
+		case 'n':
+			provided_title_name = optarg;
+			break;
+		case 'a':
+			aspect_temp = optarg;
+			break;
+		case 'r':
+			errorstrat_temp=optarg;
 			break;
 
-		case '?':
-			usage();
-			break;
-		case 'h':
-			usage();
-			break;
 		default:
-			usage();
+			lose = true;
+			break;
 		}
 	}
 
-	if (dvd == NULL) {
-		usage();
-		exit(1);
+	if(lose || optind < argc) {
+		/* Print error message and exit.  */
+		if (optind < argc) {
+			fprintf(stderr, "%s: extra operand: %s\n", program_name, argv[optind]);
+		}
+		fprintf(stderr, "Try `%s --help' for more information.\n", program_name);
+		exit (EXIT_FAILURE);
 	}
-
-	if (targetdir == NULL && do_info == 0) {
-		usage();
-		exit(1);
-	}
-
-	if (errorstrat_temp == NULL || errorstrat_temp[0]=='a') {
-		errorstrat=STRATEGY_ABORT;
-	} else if (errorstrat_temp[0]=='b') {
-	  	errorstrat=STRATEGY_SKIP_BLOCK;
-	} else if (errorstrat_temp[0]=='m') {
-	  	errorstrat=STRATEGY_SKIP_MULTIBLOCK;
-	} else {
-	  	usage();
-		exit(1);
+	
+	if(errorstrat_temp != NULL) {
+		if(errorstrat_temp[0]=='a') {
+			errorstrat=STRATEGY_ABORT;
+		} else if(errorstrat_temp[0]=='b') {
+		  	errorstrat=STRATEGY_SKIP_BLOCK;
+		} else if(errorstrat_temp[0]=='m') {
+		  	errorstrat=STRATEGY_SKIP_MULTIBLOCK;
+		} else {
+		  	print_help();
+			exit(1);
+		}
 	}
 
 	if(verbose_temp == NULL) {
@@ -337,14 +286,14 @@ int main(int argc, char *argv[]){
 	}
 
 	if((aspect != 0) && (aspect != 3) && (do_info == 0)){
-		usage();
+		print_help();
 		exit(1);
 	}
 
 	if ( titles_temp != NULL) {
 		titles = atoi(titles_temp);
 		if ( titles < 1 ) {
-			usage();
+			print_help();
 			exit(1);
 		}
 	}
@@ -352,7 +301,7 @@ int main(int argc, char *argv[]){
 	if ( start_chapter_temp !=NULL) {
 		start_chapter = atoi(start_chapter_temp);
 		if ( start_chapter < 1 || start_chapter > 99 ) {
-			usage();
+			print_help();
 			exit(1);
 		}
 	}
@@ -360,7 +309,7 @@ int main(int argc, char *argv[]){
 	if (end_chapter_temp != NULL) {
 		end_chapter = atoi(end_chapter_temp);
 		if ( end_chapter < 1 || end_chapter > 99 ) {
-			usage();
+			print_help();
 			exit(1);
 		}
 	}
@@ -372,7 +321,7 @@ int main(int argc, char *argv[]){
 			start_chapter = 1;
 		}
 		if ( end_chapter < start_chapter ) {
-			usage();
+			print_help();
 			exit(1);
 		}
 	}
@@ -389,17 +338,17 @@ int main(int argc, char *argv[]){
 	if ( title_set_temp != NULL ) {
 		title_set = atoi(title_set_temp);
 		if ( title_set > 99 || title_set < 0 ) {
-			usage();
+			print_help();
 			exit(1);
 		}
 		do_title_set = 1;
 	}
 
 	if (do_info + do_titles + do_chapter + do_feature + do_title_set  + do_mirror > 1 ) {
-		usage();
+		print_help();
 		exit(1);
 	} else if ( do_info + do_titles + do_chapter + do_feature + do_title_set  + do_mirror == 0) {
-		usage();
+		print_help();
 		exit(1);
 	}
 #ifdef DEBUG
@@ -452,7 +401,7 @@ int main(int argc, char *argv[]){
 		}
 	} else {
 		if (mkdir(targetname, 0777) != 0) {
-			fprintf(stderr,"Failed creating target directory\n");
+			fprintf(stderr,"Failed creating target directory %s\n", targetname);
 			perror("");
 			DVDClose(_dvd);
 			exit(-1);
