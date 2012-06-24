5aa5a137f54ffe05f19d98296fa52d7e7e30b114
Benjamin Drung <benjamin.drung@gmail.com>
2012-06-24 03:13:07 +0200

    Udpate year.

--- src/dvdbackup.c
+++ src/dvdbackup.c
@@ -2,7 +2,7 @@
  * dvdbackup - tool to rip DVDs from the command line
  *
  * Copyright (C) 2002  Olaf Beck <olaf_sc@yahoo.com>
- * Copyright (C) 2008-2009  Benjamin Drung <benjamin.drung@gmail.com>
+ * Copyright (C) 2008-2012  Benjamin Drung <benjamin.drung@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
